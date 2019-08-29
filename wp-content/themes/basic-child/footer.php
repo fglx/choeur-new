f4eff765ed1f39bf040312706e7aaf676d2abaaa
fglx <fra.glorieux@gmail.com>
2019-08-29 02:22:15 +0200

    update footer, remove designers, add site title, add flower

--- wp-content/themes/basic-child/footer.php
+++ wp-content/themes/basic-child/footer.php
@@ -5,28 +5,11 @@
 
 <footer id="footer" class="<?php echo apply_filters( 'basic_footer_class', '' );?>">
 
-    <?php do_action( 'basic_before_footer_menu' ); ?>
-
-    <?php if (has_nav_menu('bottom')) : ?>
-        <div class="<?php echo apply_filters( 'basic_footer_menu_class', 'footer-menu maxwidth' );?>">
-            <?php
-            wp_nav_menu( array(
-                'theme_location' => 'bottom',
-                'menu_id' => 'footer-menu',
-                'depth' => 1,
-                'container' => false,
-                'items_wrap' => '<ul class="footmenu clearfix">%3$s</ul>'
-            ));
-            ?>
-        </div>
-    <?php endif; ?>
-
-    <?php do_action( 'basic_before_footer_copyrights' ); ?>
     <?php if ( apply_filters( 'basic_footer_copyrights_enabled', true ) ) : ?>
         <div class="<?php echo apply_filters( 'basic_footer_copyrights_class', 'copyrights maxwidth grid' );?>">
             <div class="<?php echo apply_filters( 'basic_footer_copytext_class', 'copytext col6' );?>">
                 <p id="copy">
-                    <!--noindex--><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="nofollow"><?php bloginfo('name'); ?></a><!--/noindex--> &copy; <?php echo date("Y",time()); ?>
+                    <!--noindex--><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="nofollow">La Voix en Ch&oelig;ur &agrave; C&oelig;ur</a><!--/noindex--> &copy; <?php echo date("Y",time()); ?>
                     <br/>
                     <span class="copyright-text"><?php echo basic_get_theme_option('copyright_text'); ?></span>
                     <?php if ( function_exists( 'the_privacy_policy_link' ) ) {
@@ -36,9 +19,8 @@
             </div>
 
             <div class="<?php echo apply_filters( 'basic_footer_themeby_class', 'themeby col6 tr' );?>">
-                <p id="designedby">
-                    <?php _e('Theme by', 'basic'); ?>
-                    <!--noindex--><a href="<?php echo BASIC_THEME_URI; ?>" target="_blank" rel="external nofollow"><?php _e('WP Puzzle', 'basic'); ?></a><!--/noindex-->
+                <p>
+                    <img id="footer-flower" src="/pics/flower-footer.png" alt="purple flower">
                 </p>
                 <?php $counters = basic_get_theme_option('footer_counters'); ?>
                 <div class="footer-counter"><?php echo wp_specialchars_decode( $counters, ENT_QUOTES ); ?></div>
